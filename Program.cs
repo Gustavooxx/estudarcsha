c7ac6cc5c88c30e260a74c6fe2e6440647a1c35f
Gustavooxx <gustaviixx@gmail.com>
2025-10-17 15:18:42 -0300

    terminado

--- Program.cs
+++ Program.cs
@@ -1,5 +1,4 @@
 ﻿﻿using System;
-using System.Globalization;
 
 namespace estudarcsha;
 
@@ -7,37 +6,60 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("PROGRAMA DO ALUNO");
-
-        Console.WriteLine("informe a primeira nota do aluno:");
-        double nota1 = Convert.ToDouble(Console.ReadLine());
-
-         Console.WriteLine("informe a segunda nota do aluno:");
-        double nota2 = Convert.ToDouble(Console.ReadLine());
-
-         Console.WriteLine("informe a terceira nota do aluno:");
-        double nota3 = Convert.ToDouble(Console.ReadLine());
-
-        double nota = (nota1 + nota2 + nota3) / 3;
-        string situ = Resu(nota1, nota2, nota3);
-
-        Console.WriteLine($"A nota do aluno é {nota}");
-        Console.WriteLine($"{situ}");
+        Apresentacao();
+        double diametro1 = DiametroCilindro1();
+        double altura1 = AlturaCilindro1();
+        double diametro2 = DiametroCilindro2();
+        double altura2 = AlturaCilindro2();
+        VerificarTransferencia(diametro1, altura1, diametro2, altura2);
     }
 
-    public static string Resu(double nota1, double nota2, double nota3)
+    public static void Apresentacao()
     {
-        double nota = (nota1 + nota2 + nota3) / 3;
-        string passou = "";
+        Console.WriteLine("PROGRAMA DE VERIFICAÇÃO DE TRANSFERÊNCIA DE VOLUME ENTRE CILINDROS");
+    }
 
-        if (nota > 5)
-        {
-            passou = $"aluno passou";
-        }
-        else 
-        {
-            passou = $"aluno reprovou";
-        }
-        return passou;
+    public static double DiametroCilindro1()
+    {
+        Console.WriteLine("Informe o diâmetro do primeiro cilindro:");
+        double diametro = Convert.ToDouble(Console.ReadLine());
+        return diametro;
+    }
+
+    public static double AlturaCilindro1()
+    {
+        Console.WriteLine("Informe a altura do primeiro cilindro:");
+        double altura = Convert.ToDouble(Console.ReadLine());
+        return altura;
+    }
+
+    public static double DiametroCilindro2()
+    {
+        Console.WriteLine("Informe o diâmetro do segundo cilindro:");
+        double diametro = Convert.ToDouble(Console.ReadLine());
+        return diametro;
+    }
+
+    public static double AlturaCilindro2()
+    {
+        Console.WriteLine("Informe a altura do segundo cilindro:");
+        double altura = Convert.ToDouble(Console.ReadLine());
+        return altura;
+    }
+
+    public static double CalcularVolume(double diametro, double altura)
+    {
+        double raio = diametro / 2.0;
+        return Math.PI * raio * raio * altura;
+    }
+
+    public static void VerificarTransferencia(double diametro1, double altura1, double diametro2, double altura2)
+    {
+        double volume1 = CalcularVolume(diametro1, altura1);
+        double volume2 = CalcularVolume(diametro2, altura2);
+        Console.WriteLine($"O primeiro cilindro tem volume de {volume1}");
+        Console.WriteLine($"O segundo cilindro tem volume de {volume2}");
+        bool possivel = volume1 <= volume2;
+        Console.WriteLine($"É possível transferir o primeiro para o segundo? {possivel.ToString().ToLower()}");
     }
 }
