0e603402731527be12cf2240268ed7355cf9c8b3
Gustavooxx <gustaviixx@gmail.com>
2025-10-17 13:11:30 -0300

    .

--- Program.cs
+++ Program.cs
@@ -8,15 +8,33 @@ class Program
     static void Main(string[] args)
     {
 
-    }
+        Apresentacao();
+
+        string num = pegarSinal();
+
+        Console.WriteLine(Resu(num));
+    }   
 
     public static void Apresentacao()
     {
-        
+        Console.WriteLine("PROGRAMA DO IMPAR E PAR");
+
+
     }
 
-    public static string Resu(double nota1, double nota2, double nota3)
+    public static string pegarSinal()
     {
+        Console.WriteLine("informe um numero:");
+        string num = Console.ReadLine();
+        return num;
+    }
+
+    public static string Resu(string sinal)
+    {
+        string sin = "verde";
+        bool v = sinal == sin;
+        string re = $"pode atravessar? {v}";
+        return re;
 
     }
 }
